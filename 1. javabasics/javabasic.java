e1f5ce8288a4e5f4aa0609e2c5962d49235276e3
pandeyadrash <pandeyadrash57@gmail.com>
2026-02-27 22:59:34 +0530

    updatefolder

---
+++ 1. javabasics/javabasic.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class javabasic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Uncomment the code below to use different inputs and operations
+
+        String input = sc.next();
+        System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        // float price = sc.nextFloat();
+        // System.out.println(price);
+
+        // double variables = sc.nextDouble();
+        // System.out.println(variables);
+
+        // float a = sc.nextFloat();
+        // float b = sc.nextFloat();
+        // float sum = a + b;
+        // System.out.println(sum);
+
+        // float a = sc.nextFloat();
+        // float b = sc.nextFloat();
+        // float product = a * b;
+        // System.out.println(product);
+
+        // float rad = sc.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println(area);
+
+        // Type casting
+        // float number = 99.99f;
+        // int number2 = (int) number;
+        // System.out.println(number2);
+
+        // char ch = 'a';
+        // char ch2 = 'b';
+        // int number = ch;
+        // int number2 = ch2;
+        // System.out.println(number);
+        // System.out.println(number2);
+    }
+}
--- java with dsa/1. javabasics/javapattern.java
+++
@@ -1,9 +0,0 @@
-public class javapattern {
-    public static void main(String args[]) {
-        System.out.print("* * * *\n");
-        System.out.print("* * * \n");
-        System.out.print("* * \n");
-        System.out.print("* \n");
-
-    }
-}
